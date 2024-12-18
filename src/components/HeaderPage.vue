096b89954c4184e30c9a79e9ba09341bcfcf3070
ceccobell <161962926+ceccobell@users.noreply.github.com>
2024-12-18 10:30:27 +0100

    [UPD] completed header for mobile

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -8,8 +8,17 @@ export default {}
             <div class="row">
                 <div class="col-100 h-44"></div>
                 <div class="col-100 h-40">
-                    <button class="btn">Contribuisci</button>
-                    <button class="btn">Abbonati</button>
+                    <button class="btn h-100 bg-white">Contribuisci</button>
+                    <button class="btn h-100 bg-white">Abbonati</button>
+                </div>
+                <div class="col-100 h-52">
+                    <button class="bars text-red h-100 bg-white">
+                        <i class="fa-solid fa-bars"></i>
+                    </button>
+                    <input class="h-100" type="text" />
+                    <button class="search text-red h-100 bg-white">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
                 </div>
             </div>
         </div>
@@ -25,9 +34,11 @@ export default {}
     height: 40px;
 }
 
+.h-52 {
+    height: 52px;
+}
+
 .btn {
-    height: 100%;
-    background-color: white;
     width: 50%;
     border-width: 1px 0;
     border-style: solid;
@@ -36,5 +47,37 @@ export default {}
 
 .btn:first-child {
     border-right: 1px solid black;
+}
+
+.col-100.h-52 {
+    display: flex;
+}
+
+.h-52 input {
+    flex-grow: 1;
+}
+
+.bars {
+    border-left: 0;
+    border-top: 0;
+    border-right: 1px solid black;
+    border-bottom: 1px solid black;
+    padding: 0 20px;
+    cursor: pointer;
+}
+
+.search {
+    border-left: 1px solid black;
+    border-top: 0;
+    border-right: 0;
+    border-bottom: 1px solid black;
+    padding: 0 20px;
+}
+
+input {
+    border-top: 0;
+    border-left: 0;
+    border-right: 0;
+    border-bottom: 1px solid black;
 }
 </style>
